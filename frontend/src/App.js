77b90c7b42266fbefc1798a7816480af1dcf3205
AlejandroSimoncini <141671009+AlejandroSimoncini@users.noreply.github.com>
2025-05-30 12:54:44 -0300

    Actualizacion del home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Home from './Home'; // una página a la que redirigís luego
+
+import Login from './login/components/LoginForm'; // Ajustá esto si tu Login está en otra carpeta
+import Home from './home/pages/HomePage'; // Ajustá esto si tu Home está en otra carpeta
+import ActivityDetail from './home/activityDetails/pages/ActivityDetail';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/home/actividad/:id" element={<ActivityDetail/>}/>
       </Routes>
     </Router>
   );
